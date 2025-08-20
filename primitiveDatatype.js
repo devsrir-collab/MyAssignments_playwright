2c8b3ade5c07ad514e46a72fd6592e7768bba706
Devisri <devsri.r@gmail.com>
2025-08-20 18:51:15 +0530

    Add files via upload

---
+++ primitiveDatatype.js
@@ -0,0 +1,23 @@
+//  String create a variable city and print I live in <city>    
+const city ="Chennai"
+console.log("I live in " +city)
+console.log(typeof city)
+
+//Number mark 85 print mark +10 
+let mark = 85
+console.log(mark + 10)
+console.log(typeof mark)
+
+//boolean
+let ifWeekeend = true
+if (ifWeekeend == true){
+console.log("Relax")
+}else{
+ 
+    console.log("Work")
+}
+console.log(typeof ifWeekeend)
+
+// undefined
+var futureGoal
+console.log(futureGoal)
